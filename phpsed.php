611b39c25aedb04a4e4890342f83e6968d2017f8
NoahO <noah.odonoghue@gmail.com>
2019-08-14 10:56:54 +0700

    Fix minor issue in phpsed

--- phpsed.php
+++ phpsed.php
@@ -1,6 +1,6 @@
 <?php
     $re = $argv[1];
-    $subst = $argv[2];
+    $subst = str_replace("\\n", "\n", $argv[2]);
     $str = file_get_contents($argv[3]);
 
     $result = preg_replace($re, $subst, $str);
